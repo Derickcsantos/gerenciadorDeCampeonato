c8edc31e33ff875fd92ed7ce58b242135451c9f7
Derick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2024-10-24 20:25:09 -0300

    Add files via upload

--- view/controller.js
+++ view/controller.js
@@ -1,3 +1,56 @@
-c
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 
+app.use(bodyParser.json());
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+const models = require('./models');
+
+app.get('/', (req, res)=>{
+    res.send('Servidor backend já está rodando!');
+});
+
+let usuario = models.Usuario;
+
+app.get('/create', async(req, res)=>{
+    let create = await usuario.create({
+        nome: req.body.nome,
+        password: req.body.password,
+        createdAT: new Date(),
+        updateAT: new Date()
+    });
+});
+
+app.get('/read', async(req, res)=>{
+    let read=await user.findALL({
+        raw:true,
+    });
+});
+
+app.get('/update', async(req, res)=>{
+    let update=await user.findByPk(4).then((response)=>{
+        response.name="Dérick";
+        response.save();
+    }) ;
+    console.log(update);
+});
+
+app.get('/delete', async(req, res)=>{
+    user.destroy({
+        where: {id:2}
+    });
+});
+
+app.post('/login', async(req, res)=>{
+    let response=await user.findOne({
+        where:{name:req.body.name, password: req.body.password}
+    });
+    if(response === null){
+        req.send(JSON.stringify('error'));
+    }else{
+        res.send(response);
+    }
+});
